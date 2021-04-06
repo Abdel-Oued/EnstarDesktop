f8bd6cf3bbf9dde6bd42c124f68cbb127e6d8246
$Elsie <72439592+Elsie-Day@users.noreply.github.com>
2021-04-06 18:46:14 +0200

    Ajout CSS

--- src/main/java/fr/ensta/client/AccueilClient.java
+++ src/main/java/fr/ensta/client/AccueilClient.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class AccueilClient extends Application {
 
@@ -15,11 +18,14 @@ public class AccueilClient extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("/fr.ensta.client/mainClient.fxml"));
-        root.setStyle("-fx-background-color: aqua");
+        //root.setStyle("-fx-background-image: url(Logo.png)");
         Scene scene = new Scene(root,800,500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Enstar Desktop");
+        //primaryStage.getIcons().add(new Image("src/main/resources/Icon/ENSTA_LOGO.png"));
         primaryStage.show();
+
+        //scene.getStylesheets().add(getClass().getResource("/java/resources/fr.ensta.client/welcomePage.css").toExternalForm());
 
         primaryStage.setOnCloseRequest((event) -> {
             MainClientController.close(event);
